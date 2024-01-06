4090a153083ac30c7b398669ec6d21554bb7eb19
h2v9696 <h2v9696@gmail.com>
2024-01-07 00:57:42 +0900

    feat: read only attribute

---
+++ Editor/ReadOnlyAttribute.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace H2V.ExtensionsCore.Editor
+{
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}
