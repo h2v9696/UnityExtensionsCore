c370df064a14a75436387fea5ff4fc082d2d9b3d
h2v9696 <h2v9696@gmail.com>
2024-01-14 19:30:24 +0900

    feat: easier object pool manager and creation

---
+++ Runtime/ObjectPool/BaseObjectPool.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace H2V.ExtensionsCore.ObjectPool
+{
+    public abstract class BaseObjectPool<TItem> : MonoBehaviour where TItem : MonoBehaviour
+    {
+        [SerializeField] protected TItem _prefab;
+
+        // Performane is better if this is false
+        [Tooltip("If order is needed item will be set at first child when being getted")]
+        [SerializeField] private bool _isOrderNeeded;
+
+        private IObjectPool<TItem> _pool;
+
+        protected virtual void Awake()
+        {
+            _pool = new ObjectPool<TItem>(OnCreateItem, OnGetItem, OnReleaseItem,
+                OnDestroyItem);
+        }
+
+        protected virtual TItem OnCreateItem()
+        {
+            var item = Instantiate(_prefab, transform);
+            return item;
+        }
+
+        protected virtual void OnGetItem(TItem item)
+        {
+            if (_isOrderNeeded)
+                item.transform.SetAsFirstSibling();
+            item.gameObject.SetActive(true);
+        }
+
+        protected virtual void OnReleaseItem(TItem item)
+        {
+            item.gameObject.SetActive(false);
+        }
+
+        protected virtual void OnDestroyItem(TItem item)
+            => Destroy(item.gameObject);
+
+        public virtual TItem GetItem() => _pool.Get();
+
+        public virtual void ReleaseItem(TItem item)
+        {
+            if (item == null || !item.gameObject.activeInHierarchy)
+                return;
+
+            _pool.Release(item);
+        }
+
+        public virtual void ReleaseAll() => _pool.Clear();
+    }
+}
---
+++ Runtime/ObjectPool/BaseObjectPoolWithList.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace H2V.ExtensionsCore.ObjectPool
+{
+    // Has a list with current items being getted
+    public abstract class BaseObjectPoolWithList<TItem> : BaseObjectPool<TItem> 
+        where TItem : MonoBehaviour
+    {
+        protected List<TItem> _items = new();
+        public List<TItem> Items => _items;
+
+        protected override void OnGetItem(TItem item)
+        {
+            base.OnGetItem(item);
+            _items.Add(item);
+        }
+
+        protected override void OnReleaseItem(TItem item)
+        {
+            base.OnReleaseItem(item);
+            _items.Remove(item);
+        }
+
+        protected override void OnDestroyItem(TItem item)
+        {
+            _items.Remove(item);
+            base.OnDestroyItem(item);
+        }
+    }
+}
